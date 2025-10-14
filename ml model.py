448c17402c74afc01a657bacd634ae3a549a3a3e
Jadon06 <aycjadon@gmail.com>
2025-10-14 16:11:04 -0400

    Update ml model.py

--- ml model.py
+++ ml model.py
@@ -2,12 +2,15 @@ import Data_Visualization as dv
 import Data_Extraction_and_Cleaning as dec
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 dffh = dec.fighthistory1
-X = dffh[['Kd_fighter', 'Td_fighter', 'Round']]
+avg_strikes_per_round = []
+
+X = dffh[['SLPM', 'avg_Str_fighter', 'Round', 'TIM']]
 y = dffh['Str_fighter']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -17,9 +20,15 @@ lm = LinearRegression()
 #lm.fit(given, expected)
 test = lm.fit(X_train, y_train)
 
+
 #predictions
 predictions = lm.predict(X_test)
-print(predictions)
+#print(predictions)
+
+mse = mean_squared_error(y_test, predictions)
+r2 = r2_score(y_test, predictions)
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared: {r2}")
 sns.scatterplot(x=predictions, y=y_test)
 plt.xlabel('Predictions')
 plt.show()
